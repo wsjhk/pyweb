fe0a75d81bccc1276f6fd562e7d9358a70ddb2c6
wsjhk <2621570425@qq.com>
2018-10-25 18:15:50 +0800

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from pyweb import app, Response
+from middleware import token_auth
+
+
+
+@app.router(r'/login/(.*)/$')
+@token_auth
+def login(request, name):
+    return Response("<h1>Welcome, {name}!</h1>".format(name=name))
+
+
+@app.router(r'/logout/(.*)/$')
+@token_auth
+def logout(request, name):
+    return Response("<h1>Logout, {name}!</h1>".format(name=name))
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8001)
+
