c48e571b960b8d0e94bb1edcabc6fc5ca7facf43
wsjhk <2621570425@qq.com>
2018-10-26 09:06:34 +0800

    Create views.py

---
+++ views.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+class View(object):
+    methods = None
+
+    decorators = ()
+
+    @classmethod
+    def as_view(cls, name, *class_args, **class_kwargs):
+        def view(*args, **kwargs):
+            self = view.view_class(*class_args, **class_kwargs) # 反射机制
+            return self.dispatch_request(*args, **kwargs)
+
+        if cls.decorators:
+            view.__name__ = name
+            view.__module__ = cls.__module__
+            for decorator in cls.decorators:
+                view = decorator(view)
+
+        view.view_class = cls
+        view.__name__ = name
+        view.__doc__ = cls.__doc__
+        view.__module__ = cls.__module__
+        view.methods = cls.methods
+        return view
+
+    def dispatch_request(self, *args, **kwargs):
+        meth = getattr(self, 'get', None)
+        assert meth is not None, 'Unimplemented method'
+        return meth(*args, **kwargs)
+
+
+class CBV(View):
+    methods = None
+
+    decorators = ()
+
+    def get(self, request, name):
+        return "<h1>CBV, {name}!</h1>".format(name=name)
+
